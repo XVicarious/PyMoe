b5c0627ce6bf122cdfa921e80a274b12b58c5d76
Brian Maurer <bmaurer@xvicario.us>
2018-09-01 17:16:16 -0400

    make filters more friendly

--- Pymoe/Kitsu/library.py
+++ Pymoe/Kitsu/library.py
@@ -2,6 +2,7 @@ import requests
 from ..errors import *
 from .helpers import SearchWrapper
 
+
 class KitsuLib:
     def __init__(self, api, header):
         self.apiurl = api
@@ -13,13 +14,15 @@ class KitsuLib:
         list don't show what type of entry it is, you can use the filters provided
         by the Kitsu API to only select which ones you want
 
-        :param uid str: User ID to get library entries for
-        :param filters dict: Dictionary of filters for the library, keys must be in the "filter[<filter-name>]" format
-                             (https://kitsu.docs.apiary.io/#reference/user-libraries/library-entries)
+        :param uid: str: User ID to get library entries for
+        :param filters: dict: Dictionary of filters for the library
         :return: Results or ServerError
         :rtype: SearchWrapper or Exception
         """
-        r = requests.get(self.apiurl + "/users/{}/library-entries".format(uid), headers=self.header, params=filters)
+        filters = self.__format_filters(filters)
+
+        r = requests.get(self.apiurl + "/users/{}/library-entries".format(uid), headers=self.header,
+                         params=filters)
 
         if r.status_code != 200:
             raise ServerError
@@ -27,20 +30,22 @@ class KitsuLib:
         jsd = r.json()
 
         if jsd['meta']['count']:
-            return SearchWrapper(jsd['data'], jsd['links']['next'] if 'next' in jsd['links'] else None, self.header)
+            return SearchWrapper(jsd['data'],
+                                 jsd['links']['next'] if 'next' in jsd['links'] else None,
+                                 self.header)
         else:
             return None
-            
+
     def create(self, user_id, media_id, item_type, token, data):
         """
         Create a library entry for a user. data should be just the attributes.
         Data at least needs a status and progress.
         
-        :param user_id str: User ID that this Library Entry is for
-        :param media_id str: ID for the media this entry relates to
-        :param item_type str: anime, drama or manga depending
-        :param token str: OAuth token for user
-        :param data dict: Dictionary of attributes for the entry
+        :param user_id: str: User ID that this Library Entry is for
+        :param media_id: str: ID for the media this entry relates to
+        :param item_type: str: anime, drama or manga depending
+        :param token: str: OAuth token for user
+        :param data: dict: Dictionary of attributes for the entry
         :return: New Entry ID or ServerError
         :rtype: Str or Exception
         """
@@ -48,15 +53,15 @@ class KitsuLib:
             "data": {
                 "type": "libraryEntries",
                 "attributes": data,
-                "relationships":{
-                    "user":{
-                        "data":{
+                "relationships": {
+                    "user": {
+                        "data": {
                             "id": user_id,
                             "type": "users"
                         }
                     },
-                    "media":{
-                        "data":{
+                    "media": {
+                        "data": {
                             "id": media_id,
                             "type": item_type
                         }
@@ -66,53 +71,62 @@ class KitsuLib:
         }
         final_headers = self.header
         final_headers['Authorization'] = "Bearer {}".format(token)
-        
+
         r = requests.post(self.apiurl + "/library-entries", json=final_dict, headers=final_headers)
-        
+
         if r.status_code != 201:
             raise ConnectionError(r.text)
-            
+
         jsd = r.json()
-        
+
         return jsd['data']['id']
-        
+
     def update(self, eid, data, token):
         """
         Update a given Library Entry.
         
-        :param eid str: Entry ID
-        :param data dict: Attributes
-        :param token str: OAuth token
+        :param eid: str: Entry ID
+        :param data: dict: Attributes
+        :param token: str: OAuth token
         :return: True or ServerError
         :rtype: Bool or Exception
         """
         final_dict = {"data": {"id": eid, "type": "libraryEntries", "attributes": data}}
         final_headers = self.header
         final_headers['Authorization'] = "Bearer {}".format(token)
-        
-        r = requests.patch(self.apiurl + "/library-entries/{}".format(eid), json=final_dict, headers=final_headers)
-        
+
+        r = requests.patch(self.apiurl + "/library-entries/{}".format(eid), json=final_dict,
+                           headers=final_headers)
+
         if r.status_code != 200:
             raise ConnectionError(r.text)
-            
+
         return True
-        
+
     def delete(self, eid, token):
         """
         Delete a library entry.
         
-        :param eid str: Entry ID
-        :param token str: OAuth Token
+        :param eid: str: Entry ID
+        :param token: str: OAuth Token
         :return: True or ServerError
         :rtype: Bool or Exception
         """
         final_headers = self.header
         final_headers['Authorization'] = "Bearer {}".format(token)
-        
+
         r = requests.delete(self.apiurl + "/library-entries/{}".format(eid), headers=final_headers)
-        
+
         if r.status_code != 204:
             print(r.status_code)
             raise ConnectionError(r.text)
-            
+
         return True
+
+    @staticmethod
+    def __format_filters(filters):
+        if filters is not None:
+            for k, v in filters.items():
+                if 'filter[' not in k:
+                    filters['filter[{}]'.format(k)] = filters.pop(k)
+        return filters
